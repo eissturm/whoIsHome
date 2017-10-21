1cb4517c42d8635789460482236f75ba86bd80c1
Andrew Meyer <andrew@eisstrum.com>
2017-10-21 11:25:53 -0500

    Adding columns to admin interface

--- src/netmon/admin.py
+++ src/netmon/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Device
 # Register your models here.
-admin.site.register(Device)
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    model = Device
+    list_display = ('name', 'known_ip', 'last_update')
+
+admin.site.register(Device, DeviceAdmin)
